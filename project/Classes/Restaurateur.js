3b626784a47b297320aa3f2952b3398000f5ac83
Tibiscuit <louis.girardeau@ynov.com>
2020-02-01 21:24:24 +0100

    up

--- project/Classes/Restaurateur.js
+++ project/Classes/Restaurateur.js
@@ -1,14 +1,44 @@
 const Person = require('./Person');
 const Restaurant = require('./Restaurant');
 const Client = require('./Client');
+const prompts = require('prompts');
 
 class Restaurateur extends Person {
-    constructor(firstname, lastname, Restaurant) {
+    constructor(firstname, lastname, restaurant) {
         super();
         super.firstname = firstname;
         super.lastname = lastname;
         this.restaurant = restaurant;
     }
 
-    
+    async Command() {
+        const {q1} = await prompts({
+            type: 'number',
+            name: 'q1',
+            message: 'Restaurateur : Ah Bonjour ! Combien êtes-Vous?',
+            validate: q1 => q1 > 0 ? true : "Ce n'est pas un nombre exact"
+        });
+        const {q2} = await prompts({
+            type: 'list',
+            name: 'q2',
+            message: "Restaurateur : Très bien, vous êtes donc "+q1+". Combien y aura t-il de menus 1 ? Et combien de menus 2 ?",
+            initial: '',
+            separator: ' ',
+            validate: q2 => (q2[0] != 0) || (q2[1] != 0) ? true : "Restaurateur : Euh... J'attend toujours.. Combien de Menu 1 et de Menu 2?"
+        });
+        if (parseInt(q2[0])+ parseInt(q2[1]) != q1) {
+            console.log("Restaurateur : J'ai du mal noter quelque chose... On recommence !");
+            this.Command();
+        }
+        else {
+            let men1 = q2[0];
+            let men2 = q2[1];
+            let menPrice = ((men1 * this.restaurant.getPriceM1())+(men2 * this.restaurant.getPriceM2()));
+            console.log('Tres bien, donc '+q2[0]+' Menu1 et '+q2[1]+' Menu2.')
+            console.log('Ca vous fera '+menPrice+" € s'il vous plait !");
+            return menPrice;
+        }
+    }
 }
+
+module.exports = Restaurateur;
