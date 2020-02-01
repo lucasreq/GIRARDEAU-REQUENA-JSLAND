96fed1e0e6b201bcb55c47ee9b710bbae02c6edf
Tibiscuit <louis.girardeau@ynov.com>
2020-02-01 21:25:08 +0100

    up

--- project/Classes/Restaurant.js
+++ project/Classes/Restaurant.js
@@ -1,12 +1,21 @@
 const Building = require('./Building');
 
-class Restaurant{
-    constructor(nom, employee, price_m1, price_m2){
+class Restaurant extends Building {
+    constructor(name, employee, price_m1, price_m2){
+        super();
         super.name = name;
         super.employee = employee;
         this.price_m1 = price_m1;
         this.price_m2 = price_m2;
     }
+
+    getPriceM1() {
+        return this.price_m1;
+    }
+
+    getPriceM2() {
+        return this.price_m2;
+    }
 }
 
-module.export = Restaurant;
+module.exports = Restaurant;
--- project/main1.js
+++ project/main1.js
@@ -3,17 +3,22 @@ const Manager = require('./Classes/Manager');
 const Cashier = require('./Classes/Cashier');
 const Attraction = require('./Classes/Attraction');
 const Restaurant = require('./Classes/Restaurant');
+const Restaurateur = require('./Classes/Restaurateur');
 
 let Billy = new Client("Billy","Croquette",100);
 let Feteduslip = new Attraction("Feteduslip",2,100,Cashier);
 let Cashierr = new Cashier("tommy","Weinstein",Feteduslip);
+let LaBaraqueAFrite = new Restaurant("LaBaraqueAFrite",Restaurateur, 2.5,4);
+let Cuisto = new Restaurateur("Gordon", "Ramsay", LaBaraqueAFrite);
 
 let tot = 0;
 
 Billy.Hello();
-Cashierr.Pay().then(tot => {
-    Billy.payAtt(tot)
-  })
+// Cashierr.Pay().then(tot => {
+//     Billy.payAtt(tot)
+//   })
+
+Cuisto.Command()
 
 // if (tot > 0){
 //     Billy.payAtt(tot)
